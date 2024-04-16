bab72b5f47f0d4ae7914d6f60b29db6f47d78579
sathiya06 <sathiyavenkat06@gmail.com>
2024-04-16 00:21:39 -0400

    Added decision tree model

--- src/test_hytune.py
+++ src/test_hytune.py
@@ -11,28 +11,15 @@ import random
 import numpy as np
 import pandas as pd
 from sklearn.linear_model import Lasso
+from sklearn.tree import DecisionTreeRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_percentage_error
 
 def create_lasso_data_set(data_set):
+ 
+    x_train, x_test, y_train, y_test = create_dataframe(data_set)
 
-    data_file = f'../data/{data_set}/{data_set}.csv'
-
-    #createing a dataframe
-    df = pd.read_csv(data_file)
-    print(df.columns.to_numpy())
-    count = 0
-    for column in df.columns.to_numpy():
-        if column.endswith('-') or column.endswith('+'):
-            break
-        count += 1
-
-    # #splitting the dataframe into train and test
-    X = df.iloc[:, :count]  
-    Y = df.iloc[:, count:]  
-    x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=100)
-
-    #Generating data
+    #Generating hyperparameter data
     all_data = []
     alpha = 1
     while alpha <= 100:
@@ -55,12 +42,55 @@ def create_lasso_data_set(data_set):
 
     wrtie_to_csv(data_set, "lasso",['Alpha', 'Max_iter', 'Tolerance', 'fit_intercept', 'positive', 'warm_start', 'selection', 'Error-'], all_data)
 
+def create_dt_regressor_data_set(data_set):
+    x_train, x_test, y_train, y_test = create_dataframe(data_set)
+
+    #Generating hyperparameter data
+    all_data = []
+    random_state = 3200
+    for criterion in ['squared_error', 'friedman_mse', 'absolute_error', 'poisson']:
+        for splitter in ['best', 'random']:
+            for min_samples_split in [ 2, 5, 10, 37, 73, 100]:
+                for min_samples_leaf in [1, 2, 5, 10, 37, 73, 100]:
+                    ccp_alpha = 0.0
+                    while ccp_alpha <= 1:
+                        max_depth = 1
+                        while max_depth <= 10000:
+                            regressor = DecisionTreeRegressor(random_state=random_state, criterion=criterion, splitter=splitter,  min_samples_split = min_samples_split, min_samples_leaf=min_samples_leaf, ccp_alpha=ccp_alpha, max_depth=max_depth)
+                            regressor.fit(x_train, y_train)
+                            y_pred = regressor.predict(x_test)
+                            error = mean_absolute_percentage_error(y_test, y_pred)
+                            print(f"Error : {error}")
+                            all_data.append([criterion, splitter, max_depth, error])
+                            max_depth *= 10
+                        ccp_alpha += 0.1
+
+    wrtie_to_csv(data_set, "decision tree",['criterion', 'splitter', 'min_samples_split', 'min_samples_leaf', 'ccp_alpha', 'max_depth', 'Error-'], all_data)
+
+
+
+def create_dataframe(data_set):
+    data_file = f'../data/{data_set}/{data_set}.csv'
+
+    #createing a dataframe
+    df = pd.read_csv(data_file)
+    print(df.columns.to_numpy())
+    count = 0
+    for column in df.columns.to_numpy():
+        if column.endswith('-') or column.endswith('+'):
+            break
+        count += 1
+
+    #splitting the dataframe into train and test
+    X = df.iloc[:, :count]  
+    Y = df.iloc[:, count:]  
+    return train_test_split(X, Y, test_size=0.2, random_state=100)
 
 def wrtie_to_csv(data_set, algorithm_name, column_names, data):
 
     ## five by five
     #randomly selecting 10000 rows from the generated data
-    selected_data = random.sample(data, 10000)
+    selected_data = random.sample(data, min(10000, len(data)))
     #dividing it into 5
     final_data = np.array_split(selected_data, 5)
 
@@ -76,7 +106,9 @@ def wrtie_to_csv(data_set, algorithm_name, column_names, data):
             for row in data:
                 writer.writerow(row)
 
-datasets = ['SS-A', 'SS-N', 'Wine_quality', 'pom3a', 'pom3c', 'dtlz2', 'dtlz3', 'dtlz4', 'dtlz5', 'dtlz6']
+# datasets = ['SS-A']
+datasets = [ 'Wine_quality', 'pom3a', 'pom3c', 'dtlz2', 'dtlz3', 'dtlz4', 'dtlz5', 'dtlz6']
 for dataset in datasets:
     print(f'-------------------------------------------------------------------------------------------{dataset}-----------------------------------------------------------------------------------------')
-    create_lasso_data_set(dataset)
+    # create_lasso_data_set(dataset)
+    create_dt_regressor_data_set(dataset)

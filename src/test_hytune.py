d867f7b9bc7ca8fb64416f87ebf93d9c023f707a
sathiya06 <sathiyavenkat06@gmail.com>
2024-04-17 13:33:22 -0400

    added utils file

--- src/test_hytune.py
+++ src/test_hytune.py
@@ -167,54 +167,6 @@ def create_knn_data_set(data_set):
 
     write_to_csv(data_set, "knn", ['N_neighbours', 'weights', 'algorithm', 'Leaf_size', 'P', 'metric', 'Error-'], all_data)
 
-
-def create_dataframe(data_set):
-    data_file = f'../data/{data_set}/{data_set}.csv'
-
-    #createing a dataframe
-    df = pd.read_csv(data_file)
-    print(df.columns.to_numpy())
-
-    # get the x and y column indexes
-    xs, ys =  [], []
-    for i, column in enumerate( df.columns.to_numpy()):
-        if column.endswith('-') or column.endswith('+'):
-            ys.append(i)
-        else:
-            xs.append(i)
-
-    # #splitting the dataframe into train and test    
-    X = df.iloc[:,xs]  
-    Y = df.iloc[:,ys]  
-    return train_test_split(X, Y, test_size=0.2, random_state=100)
-
-def write_to_csv(data_set, algorithm_name, column_names, data, already_random=False):
-
-    ## five by five
-    #shuffle data 5 times first
-    for i in range(5):
-        random.shuffle(data)
-
-    if not already_random:
-        #randomly selecting 10000 rows from the generated data
-        selected_data = random.sample(data, min(10000, len(data)))
-    else:
-        selected_data = data
-    #dividing it into 5
-    final_data = np.array_split(selected_data, 5)
-
-    # Create the directory (handles non-existent parent directories)
-    directory = os.path.dirname(f'../data/{data_set}/{algorithm_name}/')
-    os.makedirs(directory, exist_ok=True)
-
-    for i, data in enumerate(final_data):
-        file_path = f'../data/{data_set}/{algorithm_name}/{algorithm_name}_hyperparameters_{i + 1}.csv'
-        with open(file_path, mode='w', newline='') as file:
-            writer = csv.writer(file)
-            writer.writerow(column_names)
-            for row in data:
-                writer.writerow(row)
-
 # datasets = ['SS-A']
 datasets = [ 'Wine_quality', 'pom3a', 'pom3c', 'dtlz2', 'dtlz3', 'dtlz4', 'dtlz5', 'dtlz6', 'SS-A', 'SS-K']
 for dataset in datasets:

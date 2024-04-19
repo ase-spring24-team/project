f8f6d5a34d4218cf95442ffffd0db94afae434a4
ExtremeMachine12 <kwiatkowskislmartin@gmail.com>
2024-04-19 12:00:40 -0400

    More fixes...

--- src/test_hytune.py
+++ src/test_hytune.py
@@ -413,8 +413,7 @@ if __name__ == '__main__':
     random.seed(the.seed)
     # datasets = []
     datasets = [ 'SS-A', 'Wine_quality', 'pom3a', 'pom3c', 'dtlz2', 'dtlz3', 'dtlz4', 'dtlz5', 'dtlz6', 'SS-K']
-    datasets = ['dtlz6']
-    optimization_algos = ['lasso', 'knn', 'ElasticNet', 'decision tree']
+    ml_algos = ['random_forest', 'lasso', 'knn', 'ElasticNet', 'decision tree']
     #for dataset in datasets:
         #print(f'-------------------------------------------------------------------------------------------{dataset}-----------------------------------------------------------------------------------------')
         #create_lasso_data_set(dataset)
@@ -425,5 +424,5 @@ if __name__ == '__main__':
 
     # time to optimize and run stats
     for dataset in datasets:
-        for algo_name in optimization_algos:
+        for algo_name in ml_algos:
             ranking_stats(dataset, algo_name)
